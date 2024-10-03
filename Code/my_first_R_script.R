1507d90c54f6397f9bdb9d1c49b5c49326730ef1
Duccio Rocchini <ducciorocchini@gmail.com>
2024-10-03 15:21:09 +0200

    Update my_first_R_script.R

--- Code/my_first_R_script.R
+++ Code/my_first_R_script.R
@@ -1,1 +1,21 @@
+# There are three important concpets:
+# 1. objects
+# 2. the assignment (<-)
+# 3. comments (like this one!)
+
+2 + 3 # this is my first operation in R
+
+# It is important to make use of objects
+cato <- 2 + 3  # the result of the operation is assigned to an object called cato
+cato
+
+maria <- 4 + 2
+maria
+
+cato + maria
+
+cato ^ maria
+
+(cato + maria) ^ cato
+
 
