cdd2307dcee197bcde89dd12e0526a7263ba967e
Duccio Rocchini <ducciorocchini@gmail.com>
2024-10-17 15:52:48 +0200

    Update 03_population_distribution.R

--- Code/03_population_distribution.R
+++ Code/03_population_distribution.R
@@ -21,7 +21,35 @@ par(mfrow=c(1,2))
 plot(rana)
 plot(pres)
 
-# absence
-# par pres abs
-# pres + abs
-# 
+# Exercise: select data from rana with only absences
+abse <- rana[rana$Occurrence==0]
+plot(abse)
+
+# Exercise: plot in a multiframe presences beside absences
+par(mfrow=c(1,2))
+plot(pres)
+plot(abse)
+
+# Exercise: plot in a multiframe presences on top of absences
+par(mfrow=c(2,1))
+plot(pres)
+plot(abse)
+
+# Excercise: plot the presences in blue together with absences in red
+dev.off()
+plot(pres, col="blue", pch=19, cex=2)
+points(abse, col="red", pch=19, cex=2)
+
+# Covariates
+elev <- system.file("external/elevation.asc", package="sdm")
+# [1] "/usr/local/lib/R/site-library/sdm/external/elevation.asc"
+
+elevmap <- rast(elev)
+
+# Exercise: change the colors of the elevation map by the colorRampPalette function
+cl <- colorRampPalette(c("green","hotpink","mediumpurple"))(100)
+plot(elevmap, col=cl)
+
+# Exercise: plot the presnces together with elevation map
+points(pres, pch=19)
+
