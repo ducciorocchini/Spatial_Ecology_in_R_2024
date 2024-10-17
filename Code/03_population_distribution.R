1d0b5de4d3c776649e6dc8ebbe29e9100b7e7ffe
Duccio Rocchini <ducciorocchini@gmail.com>
2024-10-17 15:03:14 +0200

    Update 03_population_distribution.R

--- Code/03_population_distribution.R
+++ Code/03_population_distribution.R
@@ -3,3 +3,25 @@
 
 library(sdm)
 library(terra)
+
+file <- system.file("external/species.shp", package="sdm")
+# [1] "/usr/local/lib/R/site-library/sdm/external/species.shp"
+
+rana <- vect(file)
+rana
+
+rana$Occurrence
+
+plot(rana)
+
+pres <- rana[rana$Occurrence==1]
+
+# Exercise: plot in a multiframe the rana dataset beside the pres dataset
+par(mfrow=c(1,2))
+plot(rana)
+plot(pres)
+
+# absence
+# par pres abs
+# pres + abs
+# 
