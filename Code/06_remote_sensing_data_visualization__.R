cabc7852b18c8b0dbb5ff0b2db846d31cf37bc65
Duccio Rocchini <ducciorocchini@gmail.com>
2024-11-20 15:41:40 +0100

    Update 06_remote_sensing_data_visualization__.R

--- Code/06_remote_sensing_data_visualization__.R
+++ Code/06_remote_sensing_data_visualization__.R
@@ -41,6 +41,38 @@ plot(b8, col=cl)
 sentstack <- c(b2, b3, b4, b8)
 plot(sentstack, col=cl)
 
+# Plotting one layer
+dev.off()
+plot(sentstack[[1]], col=cl)
+plot(sentstack[[4]], col=cl)
+
+# Multiframe with different color palette
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(b2, col=clb)
+
+# Exercise: apply the same concept to the green band (b3)
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(b3, col=clg)
+
+# Plotting red band (b4)
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(b4, col=clr)
+
+# Plotting the NIR band (b8)
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=cln)
+
+# RGB plotting
+# sentstack[[1]] blue
+# sentstack[[2]] green
+# sentstack[[3]] red
+# sentstack[[4]] NIR
+
+dev.off()
+im.plotRGB(sentstack, r=3, g=2, b=1) # natural color image
+im.plotRGB(sentstack, r=4, g=3, b=2) # false color image
 
 
 
